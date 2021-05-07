0e391674e756df15fd7f9b0c8042455e4a029446
yt249 <yt249@gmail.com>
2021-05-06 22:41:40 -0400

    store authentication tokens using authTokens

--- app.js
+++ app.js
@@ -91,3 +91,36 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+const generateAuthToken = () => {
+    return crypto.randomBytes(30).toString('hex');
+};
+
+const authTokens = {};
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = getHashedPassword(password);
+
+    const user = users.find(u => {
+        return u.email === email && hashedPassword === u.password
+    });
+
+    if (user) {
+        const authToken = generateAuthToken();
+
+        // Store authentication token
+        authTokens[authToken] = user;
+
+        // Setting the auth token in cookies
+        res.cookie('AuthToken', authToken);
+
+        // Redirect user to the protected page
+        res.redirect('/protected');
+    } else {
+        res.render('login', {
+            message: 'Invalid username or password',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
