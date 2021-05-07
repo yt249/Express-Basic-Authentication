8b530f50260e397298173f4a5b5f457a0a8d35c5
yt249 <yt249@gmail.com>
2021-05-06 22:33:15 -0400

    basic configuration

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const app = express();
+
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// Our requests handlers will be implemented here...
+
+app.listen(3000);
